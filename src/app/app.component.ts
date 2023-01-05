7fb891139b30e13b22fe9e22e1ae6bcb74632fbc
Darwin Rojas <drojasc@unprg.edu.pe>
2023-01-05 18:43:41 -0500

    se actualizaron los archivos Untracked

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    setTimeout(()=>{
+      this.peliculasEnCines=[{
+        titulo: 'Spider-Man',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99
+      },
+      {
+        titulo: 'Moana',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 300.99
+      }]
+    },3000);
+  }
+  title = 'El valor que yo quiera';
+
+  peliculasEnCines;
+  peliculasProximosEstrenos=[];
+
+  duplicarNumero(valor: number){
+    return valor *2;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
+import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-generico.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MaterialModule} from './material/material.module'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListadoPeliculasComponent,
+    ListadoGenericoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
